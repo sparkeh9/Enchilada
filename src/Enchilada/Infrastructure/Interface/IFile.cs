b93eca309d77e4db2a0a85efc19340e5263334f7
nick.briscoe <nick.briscoe@razorjam.co.uk>
2017-01-26 10:37:58 +0000

    Adding size to the interface

--- src/Enchilada/Infrastructure/Interface/IFile.cs
+++ src/Enchilada/Infrastructure/Interface/IFile.cs
@@ -1,44 +1,54 @@
 ﻿namespace Enchilada.Infrastructure.Interface
 {
-    using System;
-    using System.IO;
-    using System.Threading.Tasks;
-    using FileMode = Infrastructure.FileMode;
+   using System;
+   using System.IO;
+   using System.Threading.Tasks;
+   using FileMode = FileMode;
 
-    public interface IFile : INode, IDisposable
-    {
-        /// <summary>
-        /// Provides a Stream in read-only mode
-        /// </summary>
-        /// <returns>Stream</returns>
-        Task<Stream> OpenReadAsync();
+   public interface IFile: INode, IDisposable
+   {
+      /// <summary>
+      /// Gets the file's size
+      /// </summary>
+      long Size { get; }
 
-        /// <summary>
-        /// Asynchronously reads the file, returning a byte array
-        /// </summary>
-        /// <returns></returns>
-        Task<byte[]> ReadToEndAsync();
+      /// <summary>
+      /// Provides a Stream in read-only mode
+      /// </summary>
+      /// <returns>Stream</returns>
+      Task<Stream> OpenReadAsync();
 
-        /// <summary>
-        /// Provides a stream in write mode.
-        /// </summary>
-        /// <param name="mode">The mode in which to amend the file</param>
-        /// <returns>Stream</returns>
-        Task<Stream> OpenWriteAsync( FileMode mode = FileMode.Overwrite );
+      /// <summary>
+      /// Asynchronously reads the file, returning a byte array
+      /// </summary>
+      /// <returns></returns>
+      Task<byte[]> ReadToEndAsync();
 
-        /// <summary>
-        /// Provides a hash to check for file integrity
-        /// </summary>
-        /// <returns></returns>
-        Task<string> GetHashAsync();
+      /// <summary>
+      /// Provides a stream in write mode.
+      /// </summary>
+      /// <param name="mode">The mode in which to amend the file</param>
+      /// <returns>Stream</returns>
+      Task<Stream> OpenWriteAsync( FileMode mode = FileMode.Overwrite );
 
-        /// <summary>
-        /// Copies a file from source to target
-        /// </summary>
-        /// <param name="sourceFile"></param>
-        /// <returns></returns>
-        Task CopyFromAsync( IFile sourceFile );
+      /// <summary>
+      /// Provides a hash to check for file integrity
+      /// </summary>
+      /// <returns></returns>
+      Task<string> GetHashAsync();
 
-        Task CopyFromAsync( Stream sourceStream );
-    }
+      /// <summary>
+      /// Copies a file from source to target
+      /// </summary>
+      /// <param name="sourceFile"></param>
+      /// <returns></returns>
+      Task CopyFromAsync( IFile sourceFile );
+
+      /// <summary>
+      /// Copies data from a stream to the file
+      /// </summary>
+      /// <param name="sourceStream"></param>
+      /// <returns></returns>
+      Task CopyFromAsync( Stream sourceStream );
+   }
 }
