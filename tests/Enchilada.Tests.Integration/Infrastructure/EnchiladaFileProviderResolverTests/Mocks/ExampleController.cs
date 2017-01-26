fc31ed3dba07a02bf2a5e5a26eb020a941c0c2eb
nick.briscoe <nick.briscoe@razorjam.co.uk>
2017-01-26 11:15:33 +0000

    Adding example test

---
+++ tests/Enchilada.Tests.Integration/Infrastructure/EnchiladaFileProviderResolverTests/Mocks/ExampleController.cs
@@ -0,0 +1,30 @@
+namespace Enchilada.Tests.Integration.Infrastructure.EnchiladaFileProviderResolverTests.Mocks
+{
+   using System.IO;
+   using System.Threading.Tasks;
+   using Enchilada.Infrastructure.Interface;
+
+   public class ExampleController
+   {
+      private readonly IEnchiladaFilesystemResolver enchilada;
+
+      public ExampleController( IEnchiladaFilesystemResolver enchilada )
+      {
+         this.enchilada = enchilada;
+      }
+
+      public async Task<string> Index( string bucket, string imageName )
+      {
+         using( var downloadFileReference = enchilada.OpenFileReference( "enchilada://blob/myfile.jpg" ) )
+         {
+            using( var downloadFileStream = await downloadFileReference.OpenReadAsync() )
+            {
+               long size = downloadFileReference.Size;
+
+               var reader = new StreamReader( downloadFileStream );
+               return await reader.ReadToEndAsync();
+            }
+         }
+      }
+   }
+}
---
+++ tests/Enchilada.Tests.Integration/Infrastructure/EnchiladaFileProviderResolverTests/When_mocking_enchilada_for_use_in_an_external_system.cs
@@ -0,0 +1,30 @@
+﻿namespace Enchilada.Tests.Integration.Infrastructure.EnchiladaFileProviderResolverTests
+{
+   using System.IO;
+   using System.Text;
+   using System.Threading.Tasks;
+   using Enchilada.Infrastructure.Interface;
+   using Mocks;
+   using NSubstitute;
+   using Xunit;
+
+   public class When_mocking_enchilada_for_use_in_an_external_system
+   {
+      [ Fact ]
+      public async Task Should_provide_mockable_usage()
+      {
+         var file = Substitute.For<IFile>();
+         file.Name.Returns( "test.jpg" );
+         file.OpenReadAsync().Returns( new MemoryStream( Encoding.UTF8.GetBytes( "whatever" ) ) );
+
+         var enchilada = Substitute.For<IEnchiladaFilesystemResolver>();
+         enchilada.OpenFileReference( Arg.Any<string>() )
+                  .Returns( file );
+
+         var controllerForTest = new ExampleController( enchilada );
+         await controllerForTest.Index( "a-bucket", "jibble.jpg" );
+
+         enchilada.Received( 1 ).OpenFileReference( Arg.Any<string>() );
+      }
+   }
+}
