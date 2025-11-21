71c96d9d61746d5f92458df901d922e40cf6bc79
Nick Briscoe <nick.briscoe@razor.co.uk>
2025-11-21 20:13:29 +0000

    workflow issues

--- tests/Enchilada.Azure.Tests.Integration/AzuriteTestcontainer.cs
+++ tests/Enchilada.Azure.Tests.Integration/AzuriteTestcontainer.cs
@@ -1,22 +1,25 @@
 using System;
 using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
 using Testcontainers.Azurite;
 
 namespace Enchilada.Azure.Tests.Integration
 {
     /// <summary>
     /// Spins up an Azurite (Azure Storage emulator) Docker container for the duration of the test run.
-    /// The container is started lazily on first access and terminated when the process exits.
+    /// The container is started lazily on first access.
     /// </summary>
     internal static class AzuriteTestcontainer
     {
         private static readonly Lazy<Task<AzuriteContainer>> containerBuilder = new( StartContainerAsync );
 
-
         private static async Task<AzuriteContainer> StartContainerAsync()
         {
+            // Mirror the MinIO/FTP pattern: explicitly wait for the blob service port to be reachable
+            // before considering the container ready for tests.
             var container = new AzuriteBuilder()
                            .WithImage( "mcr.microsoft.com/azure-storage/azurite:latest" )
+                           .WithWaitStrategy( Wait.ForUnixContainer().UntilPortIsAvailable( 10000 ) )
                            .Build();
 
             await container.StartAsync();
--- tests/Enchilada.Azure.Tests.Integration/Properties/AssemblyInfo.cs
+++ tests/Enchilada.Azure.Tests.Integration/Properties/AssemblyInfo.cs
@@ -1,18 +1,22 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Enchilada.Azure.Tests.Unit")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Enchilada.Azure.Tests.Unit" )]
+[assembly: AssemblyTrademark( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae7a1fcc-5590-4c9e-af41-698965c6938a")]
+[assembly: Guid( "ae7a1fcc-5590-4c9e-af41-698965c6938a" )]
+
+// Match the FTP integration tests and run Azure tests sequentially within this assembly.
+[assembly: CollectionBehavior( DisableTestParallelization = true )]
